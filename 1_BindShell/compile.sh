b434d8d52f4c986e59e75ba214ebc4abb16e7b00
Milnor <Milnor@users.noreply.github.com>
2020-12-12 16:33:55 -0500

    WIP, won't compile

--- 1_BindShell/compile.sh
+++ 1_BindShell/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# First, build the hand-coded shellcode
+
 nasm -f elf32 -o $1.o $1.nasm
 
 if [ $? -eq 0 ]; then
@@ -18,3 +20,26 @@ else
     exit -1
 fi
 
+# Second, compile the machine-generated version
+
+mkdir -p build
+
+if [ $? -eq 0 ]; then
+    echo '[+] Created build directory'
+else
+    echo '[-] Failed to create build build directory'
+    exit -1
+fi
+
+cd build
+
+cmake .. && make
+
+if [ $? -eq 0 ]; then
+    echo '[+] Built shellcode generator.'
+else
+    echo '[-] Failed to build shellcode generator'
+    exit -1
+fi
+
+./bindshell 2345    # Port is configurable
