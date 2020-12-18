317ec9d57faf7bb6257dfd2cab45ad3edd39b104
Milnor <Milnor@users.noreply.github.com>
2020-12-17 19:12:34 -0500

    let me type less

--- 1_BindShell/compile.sh
+++ 1_BindShell/compile.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+PROJ_NAME="bind_shell"    # Default, to save me some typing!
+if [ $# -gt 0 ]; then
+    PROJ_NAME=$1
+fi 
+
 # First, build the hand-coded shellcode
 
-nasm -f elf32 -o $1.o $1.nasm
+nasm -f elf32 -o $PROJ_NAME.o $PROJ_NAME.nasm
 
 if [ $? -eq 0 ]; then
     echo '[+] Assembled with Nasm ... '
@@ -11,7 +16,7 @@ else
     exit -1
 fi
 
-ld -o $1 $1.o
+ld -o $PROJ_NAME $PROJ_NAME.o
 
 if [ $? -eq 0 ]; then
     echo '[+] Linking successful.'
